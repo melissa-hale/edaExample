70f98114a0661e0de387819aaad54b30d062336b
Melissa Hale <melissakayh@gmail.com>
2023-10-12 21:47:43 -0500

    fix migrations

--- packages/service1/db/index.js
+++ packages/service1/db/index.js
@@ -1,14 +1,16 @@
-const { Pool } = require('pg');
-const { POSTGRES_URL } = require('../config');
+const { Pool } = require("pg");
+const { POSTGRES_URL } = require("../config");
 
 const pool = new Pool({
-    connectionString: POSTGRES_URL,
+  connectionString: POSTGRES_URL,
 });
 
 async function insertIntoDB(table, eventData) {
-    return pool.query(`INSERT INTO ${table} (event_data) VALUES ($1)`, [eventData]);
-}
+  return pool.query(`INSERT INTO ${table} (event_data) VALUES ($1)`, [
+    eventData,
+  ]);
+};
 
 module.exports = {
-    insertIntoDB
+  insertIntoDB,
 };
--- packages/service1/migrations/1697064517255_addTables.js
+++ packages/service1/migrations/1697064517255_addTables.js
@@ -1,50 +1,40 @@
 exports.up = (pgm) => {
-    pgm.createTable("topic1", {
-      id: "id",
-      event_data: { type: "jsonb", notNull: true },
-      created_at: {
-        type: "timestamp",
-        notNull: true,
-        default: pgm.func("current_timestamp"),
-      },
-    });
-  };
-  
-  exports.down = (pgm) => {
-    pgm.dropTable("topic1");
-  };
-  
+  // topic1 table
+  pgm.createTable("topic1", {
+    id: "id",
+    event_data: { type: "jsonb", notNull: true },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
 
-  exports.up = (pgm) => {
-    pgm.createTable('topic2', {
-      id: 'id',
-      event_data: { type: "jsonb", notNull: true },
-      created_at: {
-        type: "timestamp",
-        notNull: true,
-        default: pgm.func("current_timestamp"),
-      },
-    });
-  };
-  
-  exports.down = (pgm) => {
-    pgm.dropTable('topic2');
-  };
-  
+  // topic2 table
+  pgm.createTable("topic2", {
+    id: "id",
+    event_data: { type: "jsonb", notNull: true },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
 
-  exports.up = (pgm) => {
-    pgm.createTable('topic3', {
-      id: 'id',
-      event_data: { type: "jsonb", notNull: true },
-      created_at: {
-        type: "timestamp",
-        notNull: true,
-        default: pgm.func("current_timestamp"),
-      },
-    });
-  };
-  
-  exports.down = (pgm) => {
-    pgm.dropTable('topic3');
-  };
-  
+  // topic3 table
+  pgm.createTable("topic3", {
+    id: "id",
+    event_data: { type: "jsonb", notNull: true },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("topic3");
+  pgm.dropTable("topic2");
+  pgm.dropTable("topic1");
+};
